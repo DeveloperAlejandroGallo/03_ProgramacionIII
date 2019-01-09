1245a61e8c7362df2c5124974cc907a801309617
Alejandro Gallo <40552405+AlejandroGalloSUGM@users.noreply.github.com>
2019-01-09 10:26:06 -0300

    Aclaracion
    Falta desarrollar mejor la commanda para poder laburar tranque en prog 4

--- TP/Comanda_GALLO/DB/upd ejemplo.sql
+++ TP/Comanda_GALLO/DB/upd ejemplo.sql
@@ -8,3 +8,14 @@ where codigo = '3H11K';
 
 
 select * from mesas
+
+select 
+'Total Facturado' titulo,
+sum(p.importe * p.cantidad) total
+from pedidos p 
+where p.horaFin is not null
+and p.horaInicio >= '2018-12-03 22:20:00' and p.horaFin <= '2018-12-04 03:20:00'
+
+
+
+select p.importe, p.cantidad, p.importe * p.cantidad tot from pedidos p where p.horaFin is not null
